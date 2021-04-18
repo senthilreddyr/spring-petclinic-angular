2e4a6cb06d757d7d92755d61fdada73bd54e2927
senthilreddy <r.senthilreddy@gmail.com>
2021-04-18 10:13:42 +0530

    removed roles from ansible

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,12 +2,3 @@
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt -y update
 sudo apt -y install ansible unzip
-# sudo wget https://deb.nodesource.com/setup_${node_version}.x --output-document /tmp/node_setup.sh
-# sudo chmod a+x /tmp/node_setup.sh
-# sudo sh /tmp/node_setup.sh
-# sudo apt -y install nodejs nginx
-# sudo npm install
-# sudo npm install -g @angular/cli
-# sudo sed -i -- 's#'"http://localhost:9966/petclinic/api/"'#'"${be_lb_dns}"'#g' ./src/environments/environment.prod.ts
-# cat ./src/environments/environment.prod.ts
-# ng build --prod --base-href=/ --deploy-url=/
